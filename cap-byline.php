1a61fdd8586b008809458a9f9135d501f55a0c7a
Brian Broderick <bribroder@gmail.com>
2015-09-19 02:02:04 -0400

    fixed for disable update time checkbox

--- cap-byline.php
+++ cap-byline.php
@@ -513,15 +513,13 @@ function get_cap_byline($type, $post_id) {
     } else {
         $time_format = 'F j, Y';
     }
-    // Here we check to make sure the post's post time is not the same as the posts updated time within the hour. We also check to make sure that the meta key that manually disables this function isn't true.
-    if ( get_the_modified_time('jnyH') != get_the_time('jnyH') && true == get_post_meta( $post_id, 'cap_enable_updated_time', true ) && false == get_field( 'global_disable_update_time', 'options' ) ) {
-        if ( true===get_field( 'global_display_post_time') ) {
-            $time_string = '<time class="published" datetime="%1$s">%2$s</time>';
-        }
-        $time_string .= '&nbsp;<time class="updated" datetime="%3$s">Updated: %4$s</time>';
-    } else {
-        if ( true===get_field( 'global_display_post_time') ) {
-            $time_string = '<time class="published" datetime="%1$s">%2$s</time>';
+
+    if ( true===get_field( 'global_display_post_time', 'options') ) {
+        $time_string = '<time class="published" datetime="%1$s">%2$s</time>';
+
+        // if display update time is not disabled and if the post time is not within one hour of the updated time...
+        if ( get_the_modified_time('jnyH') != get_the_time('jnyH') && true == get_post_meta( $post_id, 'cap_enable_updated_time', true ) && false == get_field( 'global_disable_update_time', 'options' ) ) {
+            $time_string .= '&nbsp;<time class="updated" datetime="%3$s">Updated: %4$s</time>';
         }
     }
 
